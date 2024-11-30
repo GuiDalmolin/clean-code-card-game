e7dd7a88f951fae273f459f0705d156e72e57acc
Gabriel Della Giustina <gabrielsdgiustina@gmail.com>
2024-11-30 12:18:45 -0300

    Liberadas classes handlers, para empare, derrota, vitoria e estouro, definição de interface handler

---
+++ Domain/Dtos/DefinirVencedoresDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+
+namespace Domain.Dtos
+{
+    public class DefinirVencedoresDto
+    {
+        public DefinirVencedoresDto(Jogador jogador, int pontuacaoJogador, int pontuacaoDealer)
+        {
+            Jogador = jogador;
+            PontuacaoJogador = pontuacaoJogador;
+            PontuacaoDealer = pontuacaoDealer;
+        }
+
+        public Jogador Jogador { get; set; }
+        public int PontuacaoJogador { get; set; }
+        public int PontuacaoDealer { get; set; }
+    }
+}
---
+++ Domain/Interfaces/ChainOfResponsability/IHandler.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Dtos;
+
+namespace Domain.Interfaces.ChainOfResponsability
+{
+    public interface IHandler
+    {
+        IHandler DefinirProximo(IHandler handler);
+        void Processar(DefinirVencedoresDto request);
+    }
+}
